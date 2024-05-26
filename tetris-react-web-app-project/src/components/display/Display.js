f3457c0e885a5d0e7a6a6f82e87d3420005e3982
Camila Lacerda Grandini <83710007+camilagrandinii@users.noreply.github.com>
2024-05-26 10:31:16 +0200

    Delete tetris-react-web-app-project directory

--- tetris-react-web-app-project/src/components/display/Display.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledDisplay } from "./styles/StyleDisplay";
-
-const Display = ({ gameOver, text }) => (
-  <StyledDisplay gameOver={gameOver}>{text}</StyledDisplay>
-);
-
-export default Display;
