b368125c91aafa163477cac8e0d9e6e2b46b7cd4
Camila Lacerda Grandini <83710007+camilagrandinii@users.noreply.github.com>
2024-05-26 10:29:49 +0200

    Delete musical-instrument-react-web-app-project directory

--- musical-instrument-react-web-app-project/src/utils/getKeyShortcut.js
+++
@@ -1,4 +0,0 @@
-export const  getKeyShortcut = (keyboardMap, note) => {
-    const keyShortcut = Object.keys(keyboardMap);
-    return keyShortcut.filter(shortcut => keyboardMap[shortcut] === note);
-  }
--- musical-instrument-react-web-app-project/src/utils/isFallbackNote.js
+++
@@ -1,5 +0,0 @@
-import NOTES from '../components/Notes';
-
-export default (note) => {
-    return NOTES.includes(note) && note.includes('#')
-}
